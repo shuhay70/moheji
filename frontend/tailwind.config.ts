728a5e3d6c58c682bf96d935db3fc04589627f07
shuhay70 <shuhei0210aaa@icloud.com>
2024-10-31 01:10:23 -0400

    First commit

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,6 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        //! 変数（今回はheadercolor）にクロームから取ってきた番号（今回は"#0D3858"）を入れる。それを「bg-headercolor」として使う
+        headercolor: "#0D3858",
+        headercolor2: "#092137",
+        footercolor: "#0D3657",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
