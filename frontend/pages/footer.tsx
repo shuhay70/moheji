80d72abbb24352af6d0f8228d6c32d7214524135
shuhay70 <shuhei0210aaa@icloud.com>
2024-10-31 01:06:33 -0400

    First commit

---
+++ frontend/pages/footer.tsx
@@ -0,0 +1,13 @@
+// pages/footer.tsx
+import React from "react";
+import Footer from "../src/components/Footer"; // 拡張子を省略
+
+const FooterPage = () => {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+};
+
+export default FooterPage;
